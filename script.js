fe21b20b1762e7905b43a6ae3bbc787228cb0f5c
Michal <ciurej.michal01@gmail.com>
2025-04-21 16:02:09 +0200

    Dodano plik z krajami i kodami telefonów (problemy z podanym wcześniej api)

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let clickCount = 0;
 
 const countryInput = document.getElementById('country');
+const countryInputCode = document.getElementById('countryCode');
 const myForm = document.getElementById('form');
 const modal = document.getElementById('form-feedback-modal');
 const clicksInfo = document.getElementById('click-count');
@@ -15,16 +16,29 @@ function handleClick() {
 
 async function fetchAndFillCountries() {
     try {
-        const response = await fetch('https://restcountries.com/v3.1/all');
+        const response = await fetch('https://countriesnow.space/api/v0.1/countries/positions');
         if (!response.ok) {
             throw new Error('Błąd pobierania danych');
         }
         const data = await response.json();
-        const countries = data.map(country => country.name.common);
-        countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
+        const countries = data.data.map(country => country.name).sort((a, b) => a.localeCompare(b));
+        countryInput.innerHTML = '<option value="">Wybierz kraj</option>' +
+            countries.map(country => `<option value="${country}">${country}</option>`).join('');
     } catch (error) {
         console.error('Wystąpił błąd:', error);
     }
+}
+
+async function fillCountryCodes() {
+    const response = await fetch('countries.json');
+
+    const data = await response.json();
+    const countryCodeSelect = document.getElementById('countryCode');
+    countryCodeSelect.innerHTML = '<option value="">Wybierz...</option>';
+    data.countries.forEach(country => {
+        const code = country.code.replace(/\s+/g, ' ').trim();
+        countryCodeSelect.innerHTML += `<option value="${code}">${code} (${country.name})</option>`;
+    });
 }
 
 function getCountryByIP() {
@@ -32,7 +46,12 @@ function getCountryByIP() {
         .then(response => response.json())
         .then(data => {
             const country = data.country;
-            // TODO inject country to form and call getCountryCode(country) function
+            const options = Array.from(countryInput.options);
+            const match = options.find(opt => opt.value === country);
+            if (match) {
+                countryInput.value = country;
+            }
+            getCountryCode(country);
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
@@ -44,29 +63,41 @@ function checkVat() {
     vatData.hidden = !vatBox.checked;
 }
 
-function getCountryCode(countryName) {
-    const apiUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+async function getCountryCode(countryName) {
+    const response = await fetch('countries.json');
+    if (!response.ok) {
+        console.error('Błąd pobierania countries.json');
+        return;
+    }
+    const data = await response.json();
 
-    fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Błąd pobierania danych');
-        }
-        return response.json();
-    })
-    .then(data => {        
-        const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        // TODO inject countryCode to form
-    })
-    .catch(error => {
-        console.error('Wystąpił błąd:', error);
-    });
+    const country = data.countries.find(
+        c => c.name.toLowerCase() === countryName.toLowerCase()
+    );
+    if (!country) {
+        console.warn('Nie znaleziono kraju:', countryName);
+        return;
+    }
+
+    const countryCodeSelect = document.getElementById('countryCode');
+    if (countryCodeSelect) {
+        countryCodeSelect.value = country.code;
+    }
 }
 
+async function changeDialCode() {
+    const countrySelect = document.getElementById('country');
+    const selectedOption = countrySelect.options[countrySelect.selectedIndex];
+    const countryName = selectedOption.text;
+    await getCountryCode(countryName);
+
+}
 
 (() => {
-    // nasłuchiwania na zdarzenie kliknięcia myszką
+    countryInput.addEventListener('change',changeDialCode);
     document.addEventListener('click', handleClick);
     vatBox.addEventListener('change', checkVat);
     fetchAndFillCountries();
+    fillCountryCodes();
+    getCountryByIP();
 })()
